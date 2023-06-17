bb416df7698a0090aa780474cf9f479fb45a8214
eliasku <deadbabe@gmail.com>
2023-06-17 10:33:58 +0300

    fix null app

--- packages/app/src/app/app_null.c.h
+++ packages/app/src/app/app_null.c.h
@@ -1,19 +1,19 @@
 #include <ek/app_native.h>
 
 int ek_app_open_url(const char* url) {
-    (void) sizeof(url);
+    (void)(url);
     return 1;
 }
 
 int ek_app_font_path(char* dest, uint32_t size, const char* font_name) {
-    (void) sizeof(dest);
-    (void) sizeof(size);
-    (void) sizeof(font_name);
+    (void)(dest);
+    (void)(size);
+    (void)(font_name);
     return 1;
 }
 
 int ek_app_share(const char* content) {
-    (void) sizeof(content);
+    (void)(content);
     return 1;
 }
 
@@ -25,14 +25,8 @@ int main(int argc, char* argv[]) {
     if (ek_app.state & EK_APP_STATE_EXIT_PENDING) {
         return ek_app.exit_code;
     }
-
-    int flags = 0;
-    if (ek_app.config.need_depth) {
-        flags |= 1;
-    }
     ek_app__notify_ready();
-    while((ek_app.state & EK_APP_STATE_EXIT_PENDING) == 0) {
-
+    while ((ek_app.state & EK_APP_STATE_EXIT_PENDING) == 0) {
     }
     return ek_app.exit_code;
 }
